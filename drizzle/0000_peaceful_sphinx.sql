be2082ff7090fce5bf18eb5b9c884c1a90a7bc2c
Tommy Heng <tommy.seng.heng@gmail.com>
2026-05-13 00:56:06 -0400

    chore: squash migration files

--- drizzle/0000_peaceful_sphinx.sql
+++ drizzle/0000_peaceful_sphinx.sql
@@ -1,3 +1,33 @@
+CREATE TYPE "public"."chapter_type" AS ENUM('Chapter', 'Episode', 'Issue', 'Part');--> statement-breakpoint
+CREATE TYPE "public"."volume_type" AS ENUM('Volume', 'Season', 'Arc', 'Book');--> statement-breakpoint
+CREATE TABLE "accounts" (
+	"user_id" text NOT NULL,
+	"type" text NOT NULL,
+	"provider" text NOT NULL,
+	"provider_account_id" text NOT NULL,
+	"refresh_token" text,
+	"access_token" text,
+	"expires_at" integer,
+	"token_type" text,
+	"scope" text,
+	"id_token" text,
+	"session_state" text,
+	CONSTRAINT "accounts_provider_provider_account_id_pk" PRIMARY KEY("provider","provider_account_id")
+);
+--> statement-breakpoint
+CREATE TABLE "chapter_synopses" (
+	"chapter_id" integer PRIMARY KEY NOT NULL,
+	"content" text DEFAULT '' NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "chapters" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"volume_id" integer NOT NULL,
+	"display_name" text NOT NULL,
+	"idx" integer NOT NULL
+);
+--> statement-breakpoint
 CREATE TABLE "page_infobox_image_revisions" (
 	"page_id" integer NOT NULL,
 	"chapter_id" integer NOT NULL,
@@ -54,6 +84,46 @@ CREATE TABLE "page_titles" (
 	CONSTRAINT "page_titles_page_id_chapter_id_pk" PRIMARY KEY("page_id","chapter_id")
 );
 --> statement-breakpoint
+CREATE TABLE "pages" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"serial_id" integer NOT NULL,
+	"name" text NOT NULL,
+	"slug" text NOT NULL,
+	"intro_chapter_id" integer,
+	"is_home_page" boolean DEFAULT false NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "serial_admins" (
+	"user_id" text NOT NULL,
+	"serial_id" integer NOT NULL,
+	"granted_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "serial_admins_user_id_serial_id_pk" PRIMARY KEY("user_id","serial_id")
+);
+--> statement-breakpoint
+CREATE TABLE "serial_authors" (
+	"serial_id" integer NOT NULL,
+	"name" text NOT NULL,
+	"display_order" integer NOT NULL,
+	CONSTRAINT "serial_authors_serial_id_display_order_pk" PRIMARY KEY("serial_id","display_order")
+);
+--> statement-breakpoint
+CREATE TABLE "serials" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"title" text NOT NULL,
+	"slug" text NOT NULL,
+	"description" text,
+	"splash_art_url" text,
+	"chapter_type" "chapter_type" DEFAULT 'Chapter' NOT NULL,
+	"volume_type" "volume_type" DEFAULT 'Volume' NOT NULL,
+	CONSTRAINT "serials_slug_unique" UNIQUE("slug")
+);
+--> statement-breakpoint
+CREATE TABLE "sessions" (
+	"session_token" text PRIMARY KEY NOT NULL,
+	"user_id" text NOT NULL,
+	"expires" timestamp NOT NULL
+);
+--> statement-breakpoint
 CREATE TABLE "template_infobox_sections" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"template_id" integer NOT NULL,
@@ -75,24 +145,43 @@ CREATE TABLE "templates" (
 	"has_infobox" boolean DEFAULT false NOT NULL
 );
 --> statement-breakpoint
-ALTER TABLE "category_floater_rows" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
-ALTER TABLE "category_sections" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
-ALTER TABLE "page_categories" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
-ALTER TABLE "page_floater_row_versions" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
-ALTER TABLE "page_floater_versions" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
-ALTER TABLE "page_section_versions" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
-ALTER TABLE "page_summaries" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
-DROP TABLE "category_floater_rows" CASCADE;--> statement-breakpoint
-DROP TABLE "category_sections" CASCADE;--> statement-breakpoint
-DROP TABLE "page_categories" CASCADE;--> statement-breakpoint
-DROP TABLE "page_floater_row_versions" CASCADE;--> statement-breakpoint
-DROP TABLE "page_floater_versions" CASCADE;--> statement-breakpoint
-DROP TABLE "page_section_versions" CASCADE;--> statement-breakpoint
-DROP TABLE "page_summaries" CASCADE;--> statement-breakpoint
-ALTER TABLE "pages" DROP CONSTRAINT "pages_category_id_page_categories_id_fk";
+CREATE TABLE "user_progress" (
+	"user_id" text NOT NULL,
+	"serial_id" integer NOT NULL,
+	"chapter_id" integer NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "user_progress_user_id_serial_id_pk" PRIMARY KEY("user_id","serial_id")
+);
 --> statement-breakpoint
-ALTER TABLE "pages" ADD COLUMN "serial_id" integer NOT NULL;--> statement-breakpoint
-ALTER TABLE "pages" ADD COLUMN "slug" text NOT NULL;--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" text PRIMARY KEY NOT NULL,
+	"name" text,
+	"username" text,
+	"email" text NOT NULL,
+	"email_verified" timestamp,
+	"image" text,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_username_unique" UNIQUE("username"),
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+CREATE TABLE "verification_tokens" (
+	"identifier" text NOT NULL,
+	"token" text NOT NULL,
+	"expires" timestamp NOT NULL,
+	CONSTRAINT "verification_tokens_identifier_token_pk" PRIMARY KEY("identifier","token")
+);
+--> statement-breakpoint
+CREATE TABLE "volumes" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"serial_id" integer NOT NULL,
+	"display_name" text NOT NULL,
+	"idx" integer NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "chapter_synopses" ADD CONSTRAINT "chapter_synopses_chapter_id_chapters_id_fk" FOREIGN KEY ("chapter_id") REFERENCES "public"."chapters"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "chapters" ADD CONSTRAINT "chapters_volume_id_volumes_id_fk" FOREIGN KEY ("volume_id") REFERENCES "public"."volumes"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "page_infobox_image_revisions" ADD CONSTRAINT "page_infobox_image_revisions_page_id_pages_id_fk" FOREIGN KEY ("page_id") REFERENCES "public"."pages"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "page_infobox_image_revisions" ADD CONSTRAINT "page_infobox_image_revisions_chapter_id_chapters_id_fk" FOREIGN KEY ("chapter_id") REFERENCES "public"."chapters"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "page_infobox_revisions" ADD CONSTRAINT "page_infobox_revisions_page_id_pages_id_fk" FOREIGN KEY ("page_id") REFERENCES "public"."pages"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
@@ -108,9 +197,17 @@ ALTER TABLE "page_section_revisions" ADD CONSTRAINT "page_section_revisions_chap
 ALTER TABLE "page_sections" ADD CONSTRAINT "page_sections_page_id_pages_id_fk" FOREIGN KEY ("page_id") REFERENCES "public"."pages"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "page_titles" ADD CONSTRAINT "page_titles_page_id_pages_id_fk" FOREIGN KEY ("page_id") REFERENCES "public"."pages"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "page_titles" ADD CONSTRAINT "page_titles_chapter_id_chapters_id_fk" FOREIGN KEY ("chapter_id") REFERENCES "public"."chapters"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "pages" ADD CONSTRAINT "pages_serial_id_serials_id_fk" FOREIGN KEY ("serial_id") REFERENCES "public"."serials"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "pages" ADD CONSTRAINT "pages_intro_chapter_id_chapters_id_fk" FOREIGN KEY ("intro_chapter_id") REFERENCES "public"."chapters"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "serial_admins" ADD CONSTRAINT "serial_admins_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "serial_admins" ADD CONSTRAINT "serial_admins_serial_id_serials_id_fk" FOREIGN KEY ("serial_id") REFERENCES "public"."serials"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "serial_authors" ADD CONSTRAINT "serial_authors_serial_id_serials_id_fk" FOREIGN KEY ("serial_id") REFERENCES "public"."serials"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "template_infobox_sections" ADD CONSTRAINT "template_infobox_sections_template_id_templates_id_fk" FOREIGN KEY ("template_id") REFERENCES "public"."templates"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "template_sections" ADD CONSTRAINT "template_sections_template_id_templates_id_fk" FOREIGN KEY ("template_id") REFERENCES "public"."templates"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "templates" ADD CONSTRAINT "templates_serial_id_serials_id_fk" FOREIGN KEY ("serial_id") REFERENCES "public"."serials"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "pages" ADD CONSTRAINT "pages_serial_id_serials_id_fk" FOREIGN KEY ("serial_id") REFERENCES "public"."serials"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-CREATE UNIQUE INDEX "pages_serial_id_slug_idx" ON "pages" USING btree ("serial_id","slug");--> statement-breakpoint
-ALTER TABLE "pages" DROP COLUMN "category_id";
+ALTER TABLE "user_progress" ADD CONSTRAINT "user_progress_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "user_progress" ADD CONSTRAINT "user_progress_serial_id_serials_id_fk" FOREIGN KEY ("serial_id") REFERENCES "public"."serials"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "user_progress" ADD CONSTRAINT "user_progress_chapter_id_chapters_id_fk" FOREIGN KEY ("chapter_id") REFERENCES "public"."chapters"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "volumes" ADD CONSTRAINT "volumes_serial_id_serials_id_fk" FOREIGN KEY ("serial_id") REFERENCES "public"."serials"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+CREATE UNIQUE INDEX "pages_serial_id_slug_idx" ON "pages" USING btree ("serial_id","slug");
--- drizzle/0003_acoustic_alex_wilder.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "pages" ALTER COLUMN "intro_chapter_id" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "pages" ADD COLUMN "is_home_page" boolean DEFAULT false NOT NULL;
