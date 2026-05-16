ed2f8bb8fcba3361befbdfbd2835af1c5797fbc7
Tommy Heng <tommy.seng.heng@gmail.com>
2026-05-16 00:21:13 -0400

    done

--- src/components/navbar/SignOutButton.tsx
+++ src/components/navbar/SignOutButton.tsx
@@ -4,17 +4,15 @@ import { MenuItem } from "../ui/Menu";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { Box } from "../ui/Box";
+import { signOut } from "next-auth/react";
 
 /**
  * Client-side sign-out button. Uses next-auth/react's `signOut` (not the
  * server action) so it can be invoked from an event handler.
- *
- * @example
- * <SignOutButton />
  */
 export function SignOutButton() {
   return (
-    <MenuItem>
+    <MenuItem onClick={() => signOut()}>
       <Box className="items-center gap-2">
         <FontAwesomeIcon icon={faArrowRightFromBracket} />
         Sign out
